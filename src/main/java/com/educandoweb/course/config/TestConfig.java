33785d049751f18984631faf54a0e35340867ddf
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-10-05 11:55:54 -0300

    Oder, Instant, ISO 8601 PART 2

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
@@ -17,13 +20,22 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		User user = new User(null, "Maria Brow", "maria@gmail.com", "988888888", "123456");
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
+		Order order = new Order(null, Instant.parse("2024-06-20T19:53:07Z"), user);
+		Order order2 = new Order(null, Instant.parse("2024-07-21T03:42:10Z"), user2);
+		Order order3 = new Order(null, Instant.parse("2024-07-22T15:21:22Z"), user);
+
 		userRepository.saveAll(Arrays.asList(user, user2));
+		orderRepository.saveAll(Arrays.asList(order, order2, order3));
+
 	}
 
 }
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
@@ -79,10 +82,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public List<Order> getOrders(){
+	public List<Order> getOrders() {
 		return orders;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
